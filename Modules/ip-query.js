d4572591ba68fe896f2d55ef24fbd5d2397fa019
x-ww <xsoo@duck.com>
2025-06-08 14:06:46 +0800

    up

--- Modules/ip-query.js
+++ Modules/ip-query.js
@@ -1,9 +1,19 @@
 // 兼容 Surge 面板 webhook 调用，主动请求 IP 信息
-async function main(ctx) {
-  const url = ctx?.request?.url || ctx?.argument?.url || 'https://ip-api.com/json';
-  const response = await ctx.http.get(url);
-  const data = typeof response.body === 'string' ? JSON.parse(response.body) : response.body;
-  return `落地 IP: ${data.query}\n位置: :flag_${data.countryCode.toLowerCase()}: ${data.country} ${data.regionName} ${data.city}\n运营商: ${data.org}`;
-}
+(async () => {
+  const url = typeof $argument === 'object' && $argument.url ? $argument.url : 'https://ip-api.com/json';
+  const response = await httpAPI(url);
+  const data = typeof response === 'string' ? JSON.parse(response) : response;
+  $done({
+    title: 'IP 查询',
+    content: `落地 IP: ${data.query}\n位置: :flag_${data.countryCode.toLowerCase()}: ${data.country} ${data.regionName} ${data.city}\n运营商: ${data.org}`
+  });
+})();
 
-main(ctx);
+function httpAPI(url) {
+  return new Promise((resolve, reject) => {
+    $httpClient.get(url, (err, resp, body) => {
+      if (err) return reject(err);
+      resolve(body);
+    });
+  });
+}
