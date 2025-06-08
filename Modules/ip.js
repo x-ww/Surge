0cb6848ca7528ccc77228333bb577e6484efe4b5
x-ww <xsoo@duck.com>
2025-06-08 15:46:27 +0800

    up

--- Modules/ip.js
+++ Modules/ip.js
@@ -1,4 +1,4 @@
-// Query current IP info (using ip-api.com)
+// Query current IP info (using ip-api.com) and show DNS info if available
 ;(async () => {
   function countryFlagEmoji(countryCode) {
     if (!countryCode || countryCode.length !== 2) return '';
@@ -7,6 +7,11 @@
       .split('')
       .map(c => 127397 + c.charCodeAt());
     return String.fromCodePoint(...codePoints);
+  }
+
+  let dnsInfo = "";
+  if ($network && $network.dns) {
+    dnsInfo = $network.dns.join(", ");
   }
 
   try {
@@ -21,7 +26,7 @@
     const flag = countryFlagEmoji(countryCode);
     $done({
       title: ip,
-      content: `Location: ${flag} ${country} ${city}\nISP: ${isp}`
+      content: `Location: ${flag} ${country} ${city}\nISP: ${isp}\nDNS: ${dnsInfo || "Unknown"}`
     });
   } catch (e) {
     $done({title: "Failed", content: "Failed to fetch"});
