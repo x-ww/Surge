a5fe015f1758bb28d8ed7fed34e0995919133959
x-ww <xsoo@duck.com>
2025-06-08 13:42:27 +0800

    up

--- Modules/ip-panel.js
+++ Modules/ip-panel.js
@@ -3,7 +3,7 @@
 
 async function getLandingDetail() {
   return new Promise((resolve) => {
-    $httpClient.get('https://ipwho.is/', (err, resp, data) => {
+    $httpClient.get('https://api.ip.sb/geoip', (err, resp, data) => {
       if (err) return resolve({ ip: '查询失败', country: '', region: '', city: '', country_code: '', isp: '', flag: {} });
       try {
         const obj = JSON.parse(data);
