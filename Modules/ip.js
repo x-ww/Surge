6380cbcdf912661ddd7db9ce2a6b4396394721fa
x-ww <xsoo@duck.com>
2025-06-11 18:22:07 +0800

    up

--- Modules/ip.js
+++ Modules/ip.js
@@ -1,27 +1,16 @@
-// 查询当前 IP 信息（使用 ip.sb）
+// 查询当前 IP 信息（使用 ifconfig.co）
 ;(async () => {
-  function countryFlagEmoji(countryCode) {
-    if (!countryCode || countryCode.length !== 2) return '';
-    const codePoints = countryCode
-      .toUpperCase()
-      .split('')
-      .map(c => 127397 + c.charCodeAt());
-    return String.fromCodePoint(...codePoints);
-  }
-
   try {
-    const resp = await fetch('https://ip.sb/geoip');
+    const resp = await fetch('https://ifconfig.co/json');
     if (!resp.ok) throw new Error(`Request failed: ${resp.status}`);
     const data = await resp.json();
     const ip = data.ip || "Unknown";
     const country = data.country || "Unknown";
-    const countryCode = data.country_code || "";
     const city = data.city || "Unknown";
-    const isp = data.organization || "Unknown";
-    const flag = countryFlagEmoji(countryCode);
+    const org = data.org || "Unknown";
     $done({
       title: ip,
-      content: `Location: ${flag} ${country} ${city}\nISP: ${isp}`
+      content: `Location: ${country} ${city}\nISP: ${org}`
     });
   } catch (e) {
     $done({
